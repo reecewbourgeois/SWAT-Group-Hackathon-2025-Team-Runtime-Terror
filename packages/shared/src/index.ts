a08d4defee0f7c6f70d9fda4cb10b76fa5e3eaf5
Reece Bourgeois <25267927+reecewbourgeois@users.noreply.github.com>
2025-08-11 00:02:06 -0500

    converted to turborepo, added init db migration

---
+++ packages/shared/src/index.ts
@@ -0,0 +1,6 @@
+import { z } from "zod";
+
+export const REFRESH_HEADER = "X-Access-Token";
+export const ISSUER = "trpc-passwordless-api";
+
+export const EmailSchema = z.email().max(254, "Email too long");
