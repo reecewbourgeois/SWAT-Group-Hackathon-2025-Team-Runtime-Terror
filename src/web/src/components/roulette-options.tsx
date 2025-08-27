ed9936a6f4d91d81c30dc574ec32daaf57aee95a
Zoe Klein <89620901+zoe-klein@users.noreply.github.com>
2025-08-26 23:47:35 -0500

    [feat] edit option name & styling

--- src/web/src/components/roulette-options.tsx
+++ src/web/src/components/roulette-options.tsx
@@ -1,16 +1,39 @@
 import { useMemo, useState } from "react";
 import type { WheelDataType } from "../types/WheelDataType";
 import "./roulette.css";
-import { AiOutlineDelete, AiOutlinePlus } from "react-icons/ai";
-import { BiSearch } from "react-icons/bi";
+import { AiOutlineDelete, AiOutlineEdit, AiOutlinePlus } from "react-icons/ai";
+import { BiCheck, BiSearch } from "react-icons/bi";
+import { IoClose } from "react-icons/io5";
+
+const validateUniqueOption = (data: WheelDataType[], value: string) => {
+  return data.some(
+    (val) => val.option?.toLowerCase() === value.trim().toLowerCase()
+  );
+};
+
+const validateOptionValue = (value: string) => {
+  return value.trim() === "";
+};
 
 const ErrorMessage = {
-  Exists: "Option already exists.",
+  Exists: "Name already exists.",
+  Empty: "Name cannot be empty.",
 } as const;
+
+const retrieveErrorMessage = (
+  doesOptionExist: boolean,
+  isEmpty: boolean
+): string | null => {
+  return doesOptionExist
+    ? ErrorMessage.Exists
+    : isEmpty
+    ? ErrorMessage.Empty
+    : null;
+};
 
 type Props = {
   data: WheelDataType[];
-  setData: (arg: WheelDataType[]) => void;
+  setData: React.Dispatch<React.SetStateAction<WheelDataType[]>>;
 };
 
 export const RouletteOptions = ({ data, setData }: Props) => {
@@ -24,11 +47,10 @@ export const RouletteOptions = ({ data, setData }: Props) => {
   }, [searchInput, data]);
 
   const error = useMemo((): string | null => {
-    const doesOptionExist = data.some(
-      (val) => val.option?.toLowerCase() === optionInput.trim().toLowerCase()
-    );
+    const doesOptionExist = validateUniqueOption(data, optionInput);
 
-    return doesOptionExist ? ErrorMessage.Exists : null;
+    // don't show error if empty
+    return retrieveErrorMessage(doesOptionExist, false);
   }, [optionInput, data]);
 
   const add = () => {
@@ -41,6 +63,16 @@ export const RouletteOptions = ({ data, setData }: Props) => {
 
     setData([...data, newOption]);
     setOptionInput("");
+  };
+
+  const edit = (prizeNumber: number, newName: string) => {
+    setData((prev) =>
+      prev.map((option) =>
+        option.prizeNumber === prizeNumber
+          ? { ...option, option: newName }
+          : option
+      )
+    );
   };
 
   const remove = (value: WheelDataType) => {
@@ -64,16 +96,13 @@ export const RouletteOptions = ({ data, setData }: Props) => {
       <div className="options-list-container">
         {searchedOptions.map((value) => {
           return (
-            <div className="option-container">
-              <div>
-                <label>{value.prizeNumber}. </label>
-                <label>{value.option}</label>
-              </div>
-
-              <button className="remove-button" onClick={() => remove(value)}>
-                <AiOutlineDelete />
-              </button>
-            </div>
+            <OptionRow
+              data={data}
+              edit={edit}
+              key={value.prizeNumber}
+              remove={remove}
+              value={value}
+            />
           );
         })}
       </div>
@@ -98,7 +127,7 @@ const OptionListHeader = ({
   setSearchInput,
   add,
 }: OptionListHeaderProps) => {
-  const isAddDisabled = optionInput.trim() === "" || !!error;
+  const isAddDisabled = validateOptionValue(optionInput) || !!error;
 
   return (
     <div className="option-container-header">
@@ -117,7 +146,7 @@ const OptionListHeader = ({
         <div className="input-with-label">
           <input
             onChange={(event) => setOptionInput(event.currentTarget.value)}
-            placeholder="Add Option"
+            placeholder="New Option"
             value={optionInput}
           />
 
@@ -130,6 +159,93 @@ const OptionListHeader = ({
           onClick={add}
         >
           <AiOutlinePlus />
+          Add
+        </button>
+      </div>
+    </div>
+  );
+};
+
+type OptionRowProps = {
+  value: WheelDataType;
+  edit: (prizeNumber: number, newName: string) => void;
+  remove: (arg: WheelDataType) => void;
+  data: WheelDataType[];
+};
+
+const OptionRow = ({ value, edit, remove, data }: OptionRowProps) => {
+  const optionName = value.option ?? "";
+
+  const [isEditing, setIsEditing] = useState<boolean>(false);
+  const [newNameInput, setNewNameInput] = useState<string>(optionName);
+
+  const error = useMemo((): string | null => {
+    // no error message if name was not modified from original
+    if (optionName === newNameInput) return null;
+
+    const doesOptionExist = validateUniqueOption(data, newNameInput);
+    const isEmpty = validateOptionValue(newNameInput);
+
+    return retrieveErrorMessage(doesOptionExist, isEmpty);
+  }, [data, newNameInput, optionName]);
+
+  const isConfirmDisabled =
+    validateOptionValue(newNameInput) ||
+    validateUniqueOption(data, newNameInput) ||
+    !!error;
+
+  const confirmEdit = () => {
+    edit(value.prizeNumber, newNameInput);
+    setIsEditing(false);
+  };
+
+  const closeEdit = () => {
+    setIsEditing(false);
+    setNewNameInput(optionName);
+  };
+
+  return (
+    <div className="option-container">
+      <div className="left-content">
+        <label>{value.prizeNumber}. </label>
+
+        {isEditing ? (
+          <>
+            <div className="input-with-label">
+              <input
+                onChange={(event) => setNewNameInput(event.currentTarget.value)}
+                value={newNameInput}
+              />
+
+              <label className="label">{error}</label>
+            </div>
+
+            <div className="edit-buttons-container">
+              <button
+                className="button"
+                disabled={isConfirmDisabled}
+                onClick={confirmEdit}
+              >
+                <BiCheck />
+              </button>
+
+              <button className="button" onClick={closeEdit}>
+                <IoClose />
+              </button>
+            </div>
+          </>
+        ) : (
+          <label>{value.option}</label>
+        )}
+      </div>
+
+      <div className="option-buttons">
+        <button className="edit-button" onClick={() => setIsEditing(true)}>
+          <AiOutlineEdit />
+        </button>
+
+        <button className="remove-button" onClick={() => remove(value)}>
+          <AiOutlineDelete />
         </button>
       </div>
     </div>
