87d2786a5daa6d0e094d362550d869353bb16988
Zoe Klein <89620901+zoe-klein@users.noreply.github.com>
2025-08-27 01:16:38 -0500

    [fix] styling

--- src/web/src/routes/RouletteListings.tsx
+++ src/web/src/routes/RouletteListings.tsx
@@ -4,9 +4,21 @@ import { RouletteType } from "../types/RouletteType";
 import { Sample_Roulette_Listings } from "../api/sample-roulette-listings";
 import "./rouletteListings.css";
 import { useNavigate } from "react-router-dom";
+import { setToken, trpc } from "../trpc";
 
 export const RouletteListings = () => {
   const navigate = useNavigate();
+
+  const utils = trpc.useUtils();
+
+  const logout = trpc.auth.logout.useMutation({
+    onSuccess: () => {
+      setToken(null);
+      utils.invalidate();
+      // After logout, go back to /login
+      window.location.href = "/login";
+    },
+  });
 
   const [rouletteGames, setRouletteGames] = useState<RouletteType[]>([]);
   const [searchInput, setSearchInput] = useState<string>("");
@@ -27,30 +39,44 @@ export const RouletteListings = () => {
 
   return (
     <div className="roulette-listings-page">
-      <div className="search-input">
-        <BiSearch />
-        <input
-          onChange={(event) => setSearchInput(event.currentTarget.value)}
-          placeholder="Search"
-          value={searchInput}
-        />
+      <div className="header">
+        <button
+          onClick={() => {
+            logout.mutate();
+          }}
+          type="button"
+        >
+          Logout
+        </button>
       </div>
 
-      <div className="listing-container">
-        {searchedGames.map((game) => {
-          return (
-            <div className="list-option-container" key={game.id}>
-              <label>{game.id}. </label>
+      <div className="content-container">
+        <div className="search-input">
+          <BiSearch />
+          <input
+            className="input"
+            onChange={(event) => setSearchInput(event.currentTarget.value)}
+            placeholder="Search"
+            value={searchInput}
+          />
+        </div>
 
-              <button
-                className="link-button"
-                onClick={() => navigateToGame(game)}
-              >
-                <label className="label">{game.name}</label>
-              </button>
-            </div>
-          );
-        })}
+        <div className="listing-container">
+          {searchedGames.map((game) => {
+            return (
+              <div className="list-option-container" key={game.id}>
+                <label>{game.id}. </label>
+
+                <button
+                  className="link-button"
+                  onClick={() => navigateToGame(game)}
+                >
+                  <label className="label">{game.name}</label>
+                </button>
+              </div>
+            );
+          })}
+        </div>
       </div>
     </div>
   );
